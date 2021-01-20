ab48dd8467fd4a228655e312bb8fbf60321fc3d8
Alexander Clouter <alex@digriz.org.uk>
2021-01-20 13:34:39 +0000

    make the sub-parts accessible

--- README.md
+++ README.md
@@ -70,6 +70,76 @@ Use the following to use the parser:
     )
     AS "return uaparser(ua);";
     
-    SELECT user_agent, uaparser(user_agent) AS uaparser FROM `[PROJECT].[DATASET].[TABLE]` WHERE ...;
+    SELECT user_agent, uaparser(user_agent) AS uap FROM `[PROJECT].[DATASET].[TABLE]` WHERE ...;
 
 **N.B.** remove the comment (leading '`--`') from the `description` if you make this a non-temporary function
+
+## Partial Parsing
+
+If you only want to parse our the user agent, OS or device details, you can pick the more specific function to make the query faster:
+
+### User Agent
+
+    CREATE TEMP FUNCTION uaparser_user_agent(ua STRING)
+    RETURNS STRUCT<
+      family STRING,
+      major STRING,
+      minor STRING,
+      patch STRING
+    >
+    DETERMINISTIC
+    LANGUAGE js
+    OPTIONS (
+      -- description = "User-Agent Parser (https://gitlab.com/jimdigriz/uap-js-udf)",
+      library = [
+        "gs://mybucket/uaparser/regexes.js",
+        "gs://mybucket/uaparser/uaparser.js"
+      ]
+    )
+    AS "return uaparser.user_agent(ua);";
+    
+    SELECT user_agent, uaparser_user_agent(user_agent) AS uap_user_agent FROM `[PROJECT].[DATASET].[TABLE]` WHERE ...;
+
+### Operating System
+
+    CREATE TEMP FUNCTION uaparser_os(ua STRING)
+    RETURNS STRUCT<
+      family STRING,
+      major STRING,
+      minor STRING,
+      patch STRING,
+      patchMinor STRING
+    >
+    DETERMINISTIC
+    LANGUAGE js
+    OPTIONS (
+      -- description = "User-Agent Parser (https://gitlab.com/jimdigriz/uap-js-udf)",
+      library = [
+        "gs://mybucket/uaparser/regexes.js",
+        "gs://mybucket/uaparser/uaparser.js"
+      ]
+    )
+    AS "return uaparser.os(ua);";
+    
+    SELECT user_agent, uaparser_os(user_agent) AS uap_os FROM `[PROJECT].[DATASET].[TABLE]` WHERE ...;
+
+### Device
+
+    CREATE TEMP FUNCTION uaparser_device(ua STRING)
+    RETURNS STRUCT<
+      family STRING,
+      brand STRING,
+      model STRING
+    >
+    DETERMINISTIC
+    LANGUAGE js
+    OPTIONS (
+      -- description = "User-Agent Parser (https://gitlab.com/jimdigriz/uap-js-udf)",
+      library = [
+        "gs://mybucket/uaparser/regexes.js",
+        "gs://mybucket/uaparser/uaparser.js"
+      ]
+    )
+    AS "return uaparser.device(ua);";
+    
+    SELECT user_agent, uaparser_device(user_agent) AS uap_device FROM `[PROJECT].[DATASET].[TABLE]` WHERE ...;
--- uaparser.js
+++ uaparser.js
@@ -3,29 +3,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 function uaparser(ua) {
-    const user_agent = uaparser.user_agent(ua);
-    const os = uaparser.os(ua);
-    const device = uaparser.device(ua);
-
     return {
-        'user_agent': {
-          'family': user_agent.family,
-          'major': user_agent.major || '',
-          'minor': user_agent.minor || '',
-          'patch': user_agent.patch || ''
-        },
-        'os': {
-          'family': os.family,
-          'major': os.major || '',
-          'minor': os.minor || '',
-          'patch': os.patch || '',
-          'patchMinor': os.patchMinor || ''
-        },
-        'device': {
-          'family': device.family,
-          'brand': device.brand || '',
-          'model': device.model || ''
-        }
+        user_agent: uaparser.user_agent(ua),
+        os: uaparser.os(ua),
+        device: uaparser.device(ua)
     };
 }
 uaparser.user_agent = function(ua) {
@@ -36,16 +17,19 @@ uaparser.user_agent = function(ua) {
             const m = ua.match(re);
             return m ? {
                 family: el.family_replacement ? el.family_replacement.replace('$1', m[1]) : m[1],
-                major: el.v1_replacement || m[2],
-                minor: el.v2_replacement || m[3],
-                patch: el.v3_replacement || m[4]
+                major: el.v1_replacement || m[2] || '',
+                minor: el.v2_replacement || m[3] || '',
+                patch: el.v3_replacement || m[4] || ''
             } : false;
         };
         array[index] = fn;
         return fn;
     }
     const user_agent_default = {
-        family: 'Other'
+        family: 'Other',
+        major: '',
+        minor: '',
+        patch: ''
     };
     let user_agent;
     for (let index = 0; index < regexes.user_agent_parsers.length; index++) {
@@ -64,17 +48,21 @@ uaparser.os = function(ua) {
             const m = ua.match(re);
             return m ? {
                 family: el.os_replacement ? el.os_replacement.replace('$1', m[1]) : m[1],
-                major: el.os_v1_replacement ? el.os_v1_replacement.replace('$2', m[2]) : m[2],
-                minor: el.os_v2_replacement ? el.os_v2_replacement.replace('$3', m[3]) : m[3],
-                patch: el.os_v3_replacement ? el.os_v3_replacement.replace('$4', m[4]) : m[4],
-                patchMinor: el.os_v4_replacement ? el.os_v4_replacement.replace('$5', m[5]) : m[5]
+                major: el.os_v1_replacement ? el.os_v1_replacement.replace('$2', m[2]) : m[2] || '',
+                minor: el.os_v2_replacement ? el.os_v2_replacement.replace('$3', m[3]) : m[3] || '',
+                patch: el.os_v3_replacement ? el.os_v3_replacement.replace('$4', m[4]) : m[4] || '',
+                patchMinor: el.os_v4_replacement ? el.os_v4_replacement.replace('$5', m[5]) : m[5] || ''
             } : false;
         };
         array[index] = fn;
         return fn;
     }
     const os_default = {
-        family: 'Other'
+        family: 'Other',
+        major: '',
+        minor: '',
+        patch: '',
+        patchMinor: ''
     };
     let os;
     for (let index = 0; index < regexes.os_parsers.length; index++) {
@@ -98,15 +86,17 @@ uaparser.device = function(ua) {
             const m = ua.match(re);
             return m ? {
                 family: el.device_replacement ? replace(el.device_replacement, m) : m[1],
-                brand: el.brand_replacement ? replace(el.brand_replacement, m) : undefined,
-                model: el.model_replacement? replace(el.model_replacement, m) : m[1]
+                brand: el.brand_replacement ? replace(el.brand_replacement, m) : '',
+                model: el.model_replacement? replace(el.model_replacement, m) : m[1] || ''
             } : false;
         };
         array[index] = fn;
         return fn;
     }
     const device_default = {
-        family: 'Other'
+        family: 'Other',
+        brand: '',
+        model: ''
     };
     let device;
     for (let index = 0; index < regexes.device_parsers.length; index++) {
